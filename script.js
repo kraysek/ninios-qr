81f93c2b829b4678cbb3b30d2bf00928938a55dc
kraysek <kraysek@gmail.com>
2025-06-13 17:37:44 -0600

    todo nuevo03

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (savedTheme === "dark") {
     document.body.classList.add("dark-mode");
-    document.body.classList.remove("light-mode");
   } else {
     document.body.classList.add("light-mode");
-    document.body.classList.remove("dark-mode");
   }
 });
 
